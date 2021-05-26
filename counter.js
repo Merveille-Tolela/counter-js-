1e9f3ee60e7eb692961a7b5b0ce7db22cc33459a
Merveille-Tolela <84859407+Merveille-Tolela@users.noreply.github.com>
2021-05-26 16:52:13 +0200

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,37 @@
+// set an initial count
+let count = 0;
+// size the element
+const value = document.querySelector('#cnt');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach( function (btn) {
+  btn.addEventListener('click', function(e){
+    let styles = e.currentTarget.classList;
+
+    if (styles.contains('increase')) {
+      count++;
+    }
+
+    else if (styles.contains('decrease')) {
+      count--;
+    }
+
+    else {
+      count = 0;
+    }
+  if (count > 0) {
+    value.style.color='green'
+  }
+
+  if (count < 0) {
+    value.style.color='red'
+  }
+
+  if (count == 0) {
+    value.style.color='black'
+  }
+
+  value.textContent = count;
+
+  })
+})
